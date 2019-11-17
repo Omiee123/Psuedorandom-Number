472fd4703fcdec0e72c5eb0ff39ef19e958d1751
Omiee123 <31563932+Omiee123@users.noreply.github.com>
2019-11-17 22:25:16 +0530

    Update LCG.py

--- LCG.py
+++ LCG.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 print("Linear Congruential Generator")
 print("X(n+1) = (ax(n)+c)modM")
 a = int(input("Enter a :"))
@@ -12,38 +9,18 @@ m = int(input("Enter M :"))
 x = []
 val = int(input("Enter Initial Seed Value :"))
 
-
-# In[2]:
-
-
 x.append(val)
 temp = x[0]
 
-
-# In[3]:
-
-
 i=0
-
-
-# In[5]:
-
 
 comp = ((a*x[0])+c)%m
 x.append(comp)
-
-
-# In[7]:
-
 
 if comp == temp:
     p = 1
 else:
     i = i+1
-
-
-# In[8]:
-
 
 while temp != comp:
     comp = ((a*x[i])+c)%m
@@ -51,16 +28,5 @@ while temp != comp:
     x.append(comp)
     i = i+1
 
-
-# In[9]:
-
-
 print("The Period of LCG is "+str(i))
 print("The Random Number for the given values are"+str(x))
-
-
-# In[ ]:
-
-
-
-

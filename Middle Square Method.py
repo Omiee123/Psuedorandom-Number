3827b0196dcb9f58a6bdd1d75fa06d666eaef18f
Omiee123 <31563932+Omiee123@users.noreply.github.com>
2019-11-17 22:29:50 +0530

    Update Middle Square Method.py

--- Middle Square Method.py
+++ Middle Square Method.py
@@ -1,37 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 #Middle Digit Depends on the length of seed value
 
 n = int(input("Enter the Initial Seed Value :"))
 len_n = len(str(n))
 num = int(input("Enter the no of Random Numbers you want :"))
 
-
-# In[2]:
-
-
 print(len_n)
 output = [0]*num
 
-
-# In[3]:
-
-
 output[0]=n
-
-
-# In[4]:
-
-
-output
-
-
-# In[15]:
-
 
 for i in range(num-1):
     sq_n = output[i]*output[i]
@@ -40,9 +19,4 @@ for i in range(num-1):
     temp = str(sq_n)[len_thres:-len_thres]
     output[i+1] = int(temp)
 
-
-# In[16]:
-
-
-output
-
+print("output")
